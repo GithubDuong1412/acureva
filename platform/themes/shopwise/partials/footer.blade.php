0d0e2dbd764ee8e909d80e22959f71d7cc0d9fb2
HDuong <dohongduong260202@gmail.com>
2025-12-29 16:55:55 +0700

    Update: Footer + Header Design

--- platform/themes/shopwise/partials/footer.blade.php
+++ platform/themes/shopwise/partials/footer.blade.php
@@ -2,8 +2,9 @@
         <div class="footer_top">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-3 col-md-6 col-sm-12 dk-footer-infor pt-4">
-                        <div class="widget widget-infor">
+                    {!! dynamic_sidebar('footer_sidebar') !!}
+                    <div class="col-lg-8 col-md-6 col-sm-12 dk-footer-logo pt-4">
+                        <div class="col-lg-12 col-md-6 col-sm-12">
                             @if (theme_option('logo_footer') || theme_option('logo'))
                                 <div class="footer_logo">
                                     <a href="{{ route('public.single') }}">
@@ -11,7 +12,9 @@
                                     </a>
                                 </div>
                             @endif
-                            <p>{{ theme_option('about-us') }}</p>
+                        </div>
+                        <div class="widget col-lg-8 col-md-12">
+                            <p>{!! theme_option('about-us') !!}</p>
                         </div>
                         @if (theme_option('social_links'))
                             <div class="widget">
@@ -29,42 +32,44 @@
                                 </ul>
                             </div>
                         @endif
+                        
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 dk-footer-infor pt-4">
                         <div class="widget widget-infor">
                             <ul class="contact_info contact_info_light">
                                 @if (theme_option('company-name'))
                                     <li>
                                         <i class="ti-briefcase"></i>
-                                        <p>{{ theme_option('company-name') }}</p>
+                                        <p>{!! theme_option('company-name') !!}</p>
                                     </li>
                                 @endif
                                 @if (theme_option('address'))
                                     <li>
                                         <i class="ti-location-pin"></i>
-                                        <p>{{ theme_option('address') }}</p>
+                                        <p>{!! theme_option('address') !!}</p>
                                     </li>
                                 @endif
                                 @if (theme_option('email'))
                                     <li>
                                         <i class="ti-email"></i>
-                                        <a href="mailto:{{ theme_option('email') }}">{{ theme_option('email') }}</a>
+                                        <a href="mailto:{{ theme_option('email') }}">{!! theme_option('email') !!}</a>
                                     </li>
                                 @endif
                                 @if (theme_option('hotline'))
                                     <li>
                                         <i class="ti-mobile"></i>
-                                        <p>{{ theme_option('hotline') }}</p>
+                                        <p>{!! theme_option('hotline') !!}</p>
                                     </li>
                                 @endif
                                 @if (theme_option('support-hour'))
                                     <li>
                                         <i class="ti-headphone"></i>
-                                        <p>{{ theme_option('support-hour') }}</p>
+                                        <p>{!! theme_option('support-hour') !!}</p>
                                     </li>
                                 @endif
                             </ul>
                         </div>
                     </div>
-                    {!! dynamic_sidebar('footer_sidebar') !!}
                 </div>
             </div>
         </div>
